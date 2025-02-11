d363b0e809f81fad5c14013ac2842caee4555e16
Nitin <tayalnitin008@gmail.com>
2025-02-11 14:07:40 +0530

    cors policy added

--- src/App.js
+++ src/App.js
@@ -13,8 +13,16 @@ require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(bodyParser.json());
-app.use(cors());
-
+app.use(
+  cors({
+    origin: ALLOWED_FRONTEND_URI,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // ALLOWED_CORS_METHODS,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.options("*", cors()); // Enable preflight across-the-board
+ 
 // Routes
 app.get("/data",(req, res)=>{
   res.send("Hello");
