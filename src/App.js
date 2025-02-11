ffe88292af146a5f33d9b3139056d7f5010feee6
Kaushik Jain <kaushik.jain@intimetec.com>
2025-02-11 14:32:07 +0530

    update origin to * and corrected formatting

--- src/App.js
+++ src/App.js
@@ -4,32 +4,32 @@ const cors = require("cors");
 const authRouter = require("./routes/authRoutes");
 // const PRTrackerRouter = require("./routes/prTrackerRoutes");
 const connectDB = require("./database/dbConfig");
-const storySubmissionRoute = require("./routes/storySubmissionRoutes.js")
-const storyReviewRoute = require("./routes/storyReviewRoutes.js")
+const storySubmissionRoute = require("./routes/storySubmissionRoutes.js");
+const storyReviewRoute = require("./routes/storyReviewRoutes.js");
 
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 // Middleware
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ALLOWED_FRONTEND_URI,
+    origin: "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // ALLOWED_CORS_METHODS,
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.options("*", cors()); // Enable preflight across-the-board
- 
+
 // Routes
-app.get("/data",(req, res)=>{
+app.get("/data", (req, res) => {
   res.send("Hello");
-})
+});
 
 app.use("/api/story", storySubmissionRoute);
-app.use("/api/review", storyReviewRoute)
+app.use("/api/review", storyReviewRoute);
 // app.use("/submit", PRTrackerRouter);
 app.use("/auth", authRouter);
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
+// File not in use
+
 export const PORT = 3001;
 export const DATABASE = {
   URI: process.env.MONGO_DB_URI,
